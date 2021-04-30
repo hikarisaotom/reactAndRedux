d1c8d78fb4cd7d2c05d0b45ab7e2e8e3d2e25a42
Claudia Cortes <clau_cortes10@hotmail.com>
2021-04-30 10:15:20 -0600

    final

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 //conectar la base de datos
@@ -9,7 +9,7 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+//app.get('/', (req, res) => res.send('API Running'));
 // el segundo parametro es lo que vamos a responder cuando nos hagan un get
 
 //Definir routes
@@ -17,6 +17,15 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/post'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+//Serve static assets in prod
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
